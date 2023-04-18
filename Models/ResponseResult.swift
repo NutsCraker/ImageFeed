e5af860db792d4c04d49c83dbcc494357cc9c919
Alexander Farizanov <farizanov@gmail.com>
2023-04-18 12:13:34 +0300

    Rebuild

--- Models/ResponseResult.swift
+++ Models/ResponseResult.swift
@@ -29,3 +29,51 @@ struct Profile {
 }
 
 struct PhotoLikeResult: Codable {}
+
+struct UserResult: Codable {
+    let profileImage: ProfileImageURL
+    
+    enum CodingKeys: String, CodingKey{
+        case profileImage = "profile_image"
+    }
+}
+
+struct ProfileImageURL: Codable {
+    let small: String
+    let medium: String
+}
+
+struct PhotoResult: Decodable {
+    let id: String
+    let width: Int
+    let height: Int
+    let createdAt: String?
+    let welcomeDescription: String?
+    let urls: UrlsResult
+    let isLiked: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case width = "width"
+        case height = "height"
+        case createdAt = "created_at"
+        case welcomeDescription = "description"
+        case urls = "urls"
+        case isLiked = "liked_by_user"
+    }
+}
+
+struct UrlsResult: Codable {
+    let full: String
+    let thumb: String
+}
+
+struct Photo {
+    let id: String
+    let size: CGSize
+    let createdAt: Date?
+    let welcomeDescription: String?
+    let thumbImageURL: String
+    let largeImageURL: String
+    let isLiked: Bool
+}
